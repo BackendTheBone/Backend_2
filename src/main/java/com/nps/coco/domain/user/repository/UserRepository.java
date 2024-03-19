b9f0f2bf906e2ab850ee46361ee6d1bff5fb336c
nemin <alddlv81@gmail.com>
2024-03-20 01:41:33 +0900

    fix: 실행 오류, import 오류 수정

--- src/main/java/com/nps/coco/domain/user/repository/UserRepository.java
+++ src/main/java/com/nps/coco/domain/user/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.nps.coco.domain.user.repository;
 
+import com.nps.coco.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ssafy.study.domain.user.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
