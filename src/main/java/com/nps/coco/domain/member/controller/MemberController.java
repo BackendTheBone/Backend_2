82c1a1f2f31d448c271dac0bcf1735edb46b37dd
nemin <alddlv81@gmail.com>
2024-03-22 02:12:32 +0900

    refactor: post 응답 변경

--- src/main/java/com/nps/coco/domain/member/controller/MemberController.java
+++ src/main/java/com/nps/coco/domain/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.nps.coco.domain.member.controller;
 
 import com.nps.coco.domain.member.dto.SignUpDto;
+import com.nps.coco.domain.member.exception.DuplicateEmailException;
 import com.nps.coco.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,15 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/member")
 public class MemberController {
-
     private final MemberService memberService;
 
     @PostMapping("/signUp")
-    public ResponseEntity<SignUpDto> signup(@RequestBody SignUpDto signUpDto) {
-        SignUpDto signResponse = memberService.signUp(signUpDto);
-
-        return new ResponseEntity<>(signResponse, HttpStatus.OK);
+    public ResponseEntity<?> signup(@RequestBody SignUpDto signUpDto) {
+        try{
+            memberService.signUp(signUpDto);
+            return ResponseEntity.accepted().body("ACCEPT");
+        }
+        catch (DuplicateEmailException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("BAD");
+        }
     }
 }
